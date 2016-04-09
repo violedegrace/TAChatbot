a44ef367bbeefc9b0a586d9ae0709389ee04d74b
violedegrace <sergeantfrog.m@gmail.com>
2016-04-09 13:50:13 +0700

    Main Form Decide

--- Chatbot/Program.cs
+++ Chatbot/Program.cs
@@ -17,7 +17,7 @@ namespace Chatbot
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Inisialisasi Program
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
