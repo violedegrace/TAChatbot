3885a741d05141add5dd6509e1d01b61d3af4e72
violedegrace <Sergeantfrog.m@gmail.com>
2016-04-21 03:19:10 +0700

    Laptop.untracked

---
+++ Chatbot/ManageDBForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Chatbot
+{
+    partial class ManageDBForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxManageData = new System.Windows.Forms.GroupBox();
+            this.comboBoxDomain = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBoxLocation = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.groupBoxManageData.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxManageData
+            // 
+            this.groupBoxManageData.Controls.Add(this.buttonDelete);
+            this.groupBoxManageData.Controls.Add(this.buttonAdd);
+            this.groupBoxManageData.Controls.Add(this.textBoxLocation);
+            this.groupBoxManageData.Controls.Add(this.dataGridView1);
+            this.groupBoxManageData.Controls.Add(this.comboBoxDomain);
+            this.groupBoxManageData.Location = new System.Drawing.Point(3, 0);
+            this.groupBoxManageData.Name = "groupBoxManageData";
+            this.groupBoxManageData.Size = new System.Drawing.Size(516, 361);
+            this.groupBoxManageData.TabIndex = 0;
+            this.groupBoxManageData.TabStop = false;
+            this.groupBoxManageData.Text = "Manage Data";
+            // 
+            // comboBoxDomain
+            // 
+            this.comboBoxDomain.FormattingEnabled = true;
+            this.comboBoxDomain.Location = new System.Drawing.Point(13, 20);
+            this.comboBoxDomain.Name = "comboBoxDomain";
+            this.comboBoxDomain.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxDomain.TabIndex = 0;
+            this.comboBoxDomain.SelectedIndexChanged += new System.EventHandler(this.IndexChange);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 48);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(497, 273);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // textBoxLocation
+            // 
+            this.textBoxLocation.Location = new System.Drawing.Point(13, 331);
+            this.textBoxLocation.Name = "textBoxLocation";
+            this.textBoxLocation.Size = new System.Drawing.Size(335, 20);
+            this.textBoxLocation.TabIndex = 1;
+            this.textBoxLocation.DoubleClick += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(354, 328);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(435, 328);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 3;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(357, 368);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 4;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(439, 368);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // ManageDBForm
+            // 
+            this.AcceptButton = this.buttonAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 401);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.groupBoxManageData);
+            this.Name = "ManageDBForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "ManageDBForm";
+            this.groupBoxManageData.ResumeLayout(false);
+            this.groupBoxManageData.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxManageData;
+        private System.Windows.Forms.ComboBox comboBoxDomain;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxLocation;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ Chatbot/ManageDBForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chatbot
+{
+    public partial class ManageDBForm : Form
+    {
+        dbDataContext db;
+        tbDomain current;
+        List<tbInformasi> DataBaru;
+        public ManageDBForm(string args, dbDataContext database)
+        {
+            InitializeComponent();
+            this.Text = args;
+            db = database;
+            DataBaru = new List<tbInformasi>();
+            comboBoxDomain.DataSource = db.tbDomains.Select(x => x.Name).ToList();
+        }
+
+        private void IndexChange(object sender, EventArgs e)
+        {
+            current = db.tbDomains.Where(x => x.Name.Equals(comboBoxDomain.SelectedValue)).FirstOrDefault();
+            if (current != null)
+            {
+                if (current.Id==0)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                MessageBox.Show("Data tidak diketahui");
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxLocation.Text))
+            {
+                OpenFileDialog browse = new OpenFileDialog();
+                //browse.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+                browse.Filter = "Text Files (.txt)|*.txt";
+                browse.FilterIndex = 0;
+                browse.Multiselect = true;
+                if (browse.ShowDialog()==DialogResult.OK)
+                {
+                    foreach (var files in browse.FileNames)
+                    {
+                        string[] nama = files.Split('\\').ToArray();
+                        if (!File.Exists(Lingkungan.getDataCache() + "/" + current.Name + "/" + nama[nama.Length - 1]))
+                        {
+                            File.Copy(files, Lingkungan.getDataCache() + "/" + current.Name + "/" + nama[nama.Length - 1]);
+                            tbInformasi baru = new tbInformasi();
+                            baru.Judul = nama[nama.Length - 1].Substring(0, nama[nama.Length - 1].Length-5);
+                            DataBaru.Add(baru);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
